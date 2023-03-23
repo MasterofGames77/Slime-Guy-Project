290709738ed8126aca70d8a5ab501ea23723d640
MasterofGames77 <mgambardella16@gmail.com>
2023-03-23 13:00:50 -0400

    Initial Project Upload
    Uploading Project Files

---
+++ Assets/My Scripts/MainMenu.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject controlsScreen;
+
+    public GameObject creditsScreen;
+
+    public GameObject optionsScreen;
+
+    // Called Zero
+    void Awake()
+    {
+        Debug.Log("Awake!");
+    }
+
+    // Called First
+    void OnEnable()
+    {
+        Debug.Log("OnEnable Called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // Called Second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+    }
+
+    // Called Third
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Called when the scene is terminated
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+        Time.timeScale = 1f;
+    }
+
+    public void OpenControls()
+    {
+        controlsScreen.SetActive(true);
+    }
+
+    public void CloseControls()
+    {
+        controlsScreen.SetActive(false);
+    }
+
+    public void OpenCredits()
+    {
+        creditsScreen.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        creditsScreen.SetActive(false);
+    }
+
+    public void OpenOptions()
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting");
+    }
+}
---
+++ Assets/My Scripts/UIManager.cs
@@ -0,0 +1,147 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Platformer.Mechanics;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEditor.PackageManager;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+
+    [SerializeField] private TextMeshProUGUI levelTimerText;
+
+    //[SerializeField] private TextMeshProUGUI gemsText;
+
+    [SerializeField] private GameObject Player;
+
+    public Button restartButton;
+
+    public Button nextLevelButton;
+
+    public Button quitButton;
+
+    public GameObject levelCompleteScreen;
+
+    private float timeLeft;
+
+    //private float startTime = 0;
+
+    private bool isGameActive;
+
+    private int seconds;
+
+    //private static int gemsCollected;
+
+    private static UIManager uiManagerInstance;
+
+    //private static TokenInstance tokenInstance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (uiManagerInstance == null)
+        {
+            uiManagerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Time.timeScale = 1.0f;
+        isGameActive = true;
+        timeLeft += Time.deltaTime;
+        //gemsCollected = 0;
+        Player.gameObject.SetActive(true);
+        timerText.gameObject.SetActive(true);
+       // gemsText.gameObject.SetActive(true);
+       //gemsText.text = $"Gems: " + gemsCollected.ToString();
+        restartButton.gameObject.SetActive(false);
+        nextLevelButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameActive)
+        {
+            timeLeft += Time.deltaTime;
+            timerText.SetText("Time: " + Mathf.Round(timeLeft));
+            if (timeLeft <= 0)
+            {
+                Debug.Log("Game Over!");
+            }
+        }
+    }
+
+    /*public void CollectedGem(int gemsToAdd)
+    {
+        gemsCollected += gemsToAdd;
+        gemsText.text = $"Gems: " + gemsCollected.ToString();
+        Debug.Log("Gem Collected");
+    }*/
+
+    public void LoadNextLevel()
+    {
+        Time.timeScale = 1.0f;
+        ResetTimer();
+        timeLeft = Time.deltaTime;
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("The next level has been loaded");
+        isGameActive = true;
+        Player.gameObject.SetActive(true);
+        //gemsCollected = 0;
+        levelCompleteScreen.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        nextLevelButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1.0f;
+        ResetTimer();
+        timeLeft = Time.deltaTime;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("The Scene has been reloaded.");
+        isGameActive = true;
+        Player.gameObject.SetActive(true);
+        //gemsCollected = 0;
+        levelCompleteScreen.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(false);
+        nextLevelButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+    }
+
+    public void ResetTimer()
+    {
+        Time.timeScale = 1.0f;
+        timeLeft = 0;
+        Debug.Log("Reset the Timer");
+    }
+
+    public void LevelCompleted()
+    {
+        Time.timeScale = 0.0f;
+        levelCompleteScreen.gameObject.SetActive(true);
+        levelTimerText.gameObject.SetActive(true);
+        levelTimerText.SetText("Final Time: " + Mathf.Round(timeLeft) + " Seconds");
+        levelTimerText = timerText;
+        timerText.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
+        nextLevelButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Game");
+    }
+}
